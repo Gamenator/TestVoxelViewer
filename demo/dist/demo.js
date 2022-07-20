69096603fcaf9031abe1eba623ab1c1ee2e761b1
Vitalii Novosad <novosadvitalya@gmail.com>
2022-07-20 15:15:43 +0300

    asdsad

--- demo/dist/demo.js
+++ demo/dist/demo.js
@@ -4366,7 +4366,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = __webpack_require__("./demo/demo.js");
-/******/ 	//var __webpack_exports__ = __webpack_require__("./demo/demos.js");
 /******/ 	
 /******/ })()
 ;
