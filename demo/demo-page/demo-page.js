0ada89ee224bf8cc6d6e575ce27c2b3432827636
Vitalii Novosad <novosadvitalya@gmail.com>
2022-07-20 15:17:11 +0300

    Update demo-page.js

--- demo/demo-page/demo-page.js
+++ demo/demo-page/demo-page.js
@@ -63,7 +63,6 @@ class DemoPage extends LitElement {
           @change="${this.onModelSelected}"
         >
           <option value="deer">Deer</option>
-          <option value="monu7">Monument 7</option>
           <option value="monu8">Monument 8</option>
         </select>
       </div>
